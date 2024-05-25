72786fa601450c49bc827d401846286579fab272
Anwarul07 <mdanwarul064@gmail.com>
2024-05-25 23:56:20 +0530

    Update sscripts.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("coming soon web");
+
+let counttDate = new Date("June 20, 2024 00:00:00").getTime();
+
+let x = setTimeout(() => {
+    let currentt = new Date().getTime();
+    let distance = counttDate - currentt;
+
+
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("min").innerHTML = minutes;
+    document.getElementById("sec").innerHTML = seconds;
+
+
+    if (distance < 0) {
+        clearInterval(x)
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("min").innerHTML = "00";
+        document.getElementById("sec").innerHTML = "00";
+
+
+    }
+
+}, 1000);
